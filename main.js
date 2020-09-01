de70fb8d4741edf1d88e12f5615db50fd0893fd8
Andrea Contestabile <andreacontest@gmail.com>
2020-09-01 17:34:03 +0200

    Versione AddClass/RemoveClass

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-var hamButtonOpen = $(".header-right > a");
-var hamButtonClose = $(".hamburger-menu > a");
+var hamButtonOpen = $(".header-right > a ");
+var hamButtonClose = $(".close");
 var hamMenu = $(".hamburger-menu");
 
 hamButtonOpen.click(
   function() {
-    hamMenu.show(300);
-    //oppure hamMenu.fadeIn(300);
+    hamMenu.addClass("active");
   }
 );
 
 hamButtonClose.click(
   function() {
-    hamMenu.hide(300);
-    //oppure hamMenu.fadeOut(300);
+    hamMenu.removeClass("active");
   }
 );
 
 // $(".header-right > a").click(
 //   function() {
 //     $(".hamburger-menu").show(300);
+        //oppure hamMenu.fadeIn(300);
 //   }
 // );
 //
 // $(".hamburger-menu > a").click(
 //   function() {
 //     $(".hamburger-menu").hide(300);
+        //oppure hamMenu.fadeOut(300);
 //   }
 // );
