6724484eb09b58b3a56b3a76996c51838a71fb0d
Andrea Contestabile <andreacontest@gmail.com>
2020-09-01 16:16:45 +0200

    Hide/Show Hamburger Menu con jQuery

---
+++ main.js
@@ -0,0 +1,11 @@
+$(".header-right > a").click(
+  function() {
+    $(".hamburger-menu").show(300);
+  }
+);
+
+$(".hamburger-menu > a").click(
+  function() {
+    $(".hamburger-menu").hide(300);
+  }
+);
