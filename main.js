fa5fdad035d70877e20b592664305cce8f5f061d
Andrea Contestabile <andreacontest@gmail.com>
2020-09-01 16:21:48 +0200

    Versione con variabili

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-$(".header-right > a").click(
+var hamButtonOpen = $(".header-right > a");
+var hamButtonClose = $(".hamburger-menu > a");
+var hamMenu = $(".hamburger-menu");
+
+hamButtonOpen.click(
   function() {
-    $(".hamburger-menu").show(300);
+    hamMenu.show(300);
+    //oppure hamMenu.fadeIn(300);
   }
 );
 
-$(".hamburger-menu > a").click(
+hamButtonClose.click(
   function() {
-    $(".hamburger-menu").hide(300);
+    hamMenu.hide(300);
+    //oppure hamMenu.fadeOut(300);
   }
 );
+
+// $(".header-right > a").click(
+//   function() {
+//     $(".hamburger-menu").show(300);
+//   }
+// );
+//
+// $(".hamburger-menu > a").click(
+//   function() {
+//     $(".hamburger-menu").hide(300);
+//   }
+// );
